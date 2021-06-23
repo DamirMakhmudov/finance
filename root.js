d2e87a12b0b2e7b24ee1619a2aa67f53fb9c5c51
Damir <d.makhmudov@kazan.csoft.ru>
2021-06-23 18:33:11 +0300

    last one

--- root.js
+++ root.js
@@ -6,16 +6,17 @@ var vueObject = {
   template:
   /*html*/
   `
-  <!-- select payment method -->
-  <div class="q-pa-md fit row justify-center">
+  <div class="q-pa-md fit column justify-center">
 
-    
+    <!-- first buttons -->
     <div class="q-gutter-y-xs">
+
       <q-btn-toggle
         spread
         v-model = "paymentMethod.val"
         toggle-color = "primary"
         :options= "paymentMethodOptions"
+        v-on:click="showplaceFn();showaddressFn()"
       />
 
       <q-btn-toggle
@@ -31,38 +32,65 @@ var vueObject = {
         toggle-color = "accent"
         :options= "placeOptions"
       />
+
     </div>
 
     <!-- address -->
-    <div id="q-gutter-y-xs">
+      <q-select
+        filled
+        v-model="address.val"
+        use-input
+        input-debounce="0"
+        label="Адрес"
+        :options="addressOptions"
+        @filter="filterFn"
+        behavior="menu"
+        v-if="showaddress"
+      >
+      </q-select>
 
-    </div>
-
+    <q-input v-model="text" label="Standard" />
     
   </div>
 
   <div v-show='show' class='text-caption'>
+    <hr>
     model: {{model}}
   </div>
   `
   ,
   setup() {
-    var paymentMethod = ref(model.paymentMethod)
+    const paymentMethod = ref(model.paymentMethod)
+    const showplace = ref(false);
+    const showaddress = ref(false);
+    const addressOptions = ref(model.addressOptions);
 
-    // watch(paymentMethod.value, (val) =>{
-    //   console.log(val)
-    //   val.val = 'vdnh' ? showplace=true : showplace=false 
-    // })
+    function filterFn (val, update) {
+      if (val === '') {
+        update(() => {
+          addressOptions.value = model.addressOptions
+        })
+        return
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        addressOptions.value = model.addressOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+      })
+    }
 
-    // var showplace = ref(toggelPaymentMethod(paymentMethod))
+    function showplaceFn(){
+      showplace.value = paymentMethod.value.val == 'cash' ? true : false;
+    }
 
-    // var showplace = ref(computed(() => { return toggelPaymentMethod(paymentMethod) }));
-    var showplace = ref(computed(() => { return paymentMethod.value.val == 'cash' ? true : false }));
+    function showaddressFn(){
+      showaddress.value = paymentMethod.value.val == 'cash' ? true : false;
+    }
 
     return {
       show: false,
       // show: true,
       model: ref(model),
+
       paymentMethod,
       paymentMethodOptions: ref(model.paymentMethodOptions),
       paymentType: ref(model.paymentType),
@@ -70,6 +98,12 @@ var vueObject = {
       place: ref(model.place),
       placeOptions: ref(model.placeOptions),
       showplace,
+      showplaceFn,
+      address: ref(model.address),
+      addressOptions,
+      showaddress,
+      showaddressFn,
+      filterFn
     }
   }
 }
@@ -84,3 +118,12 @@ app.use(Quasar, {
 });
 
 Quasar.lang.set(Quasar.lang.ru);
+
+ // watch(paymentMethod.value, (val) =>{
+//   console.log(val)
+//   val.val = 'vdnh' ? showplace=true : showplace=false 
+// })
+
+// var showplace = ref(toggelPaymentMethod(paymentMethod))
+// var showplace = ref(computed(() => { return toggelPaymentMethod(paymentMethod) }));
+// const showplace = ref(computed(() => { return paymentMethod.value.val == 'cash' ? true : false }));
