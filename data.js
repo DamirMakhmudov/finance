3230cbc483cb1c1e2b01325e79e8fadec10ad858
Damir <d.makhmudov@kazan.csoft.ru>
2021-06-30 19:05:56 +0300

    dd

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-var model = {
+var model = {"val":{
   paymentMethod: { 'val': "" },
 
   paymentMethodOptions: [
@@ -59,5 +59,69 @@ var model = {
     {'label':'Оплата посреднику','needaddress':false},
     {'label':'Рабочие расходы по объекту','needaddress':false}
   ]
-
 }
+}
+
+var origin = {"val":{
+  paymentMethod: { 'val': "" },
+
+  paymentMethodOptions: [
+    { label: 'Нал', value: 'cash' },
+    { label: 'Безнал', value: 'cashless' }
+  ],
+
+  paymentType: { 'val': "" },
+  
+  paymentTypeOptions: [
+    { label: 'Приход', value: 'income' },
+    { label: 'Расход', value: 'outcome' }
+  ],
+
+  place: { 'val': "" },
+  
+  placeOptions: [
+    { label: 'ВДНХ', value: 'vdnh' },
+    { label: 'ВП', value: 'vp' }
+  ],
+
+  address: {'val': ''},
+  
+  addressOptions:[
+    "г.Москва, Волоколамское ш., д.71, корп.1, кв.548",
+    "г.Москва, ул.Старокачаловская, д.1, к.1, кв.100",
+    "г.Москва, ул.Вешняковская, д.10, кв.39",
+    "г.Москва, Шарикоподшипниковская ул., д.13, стр.24",
+    "г.Москва, ул.Арбат, д.10, кв.39",
+    "г.Москва, ул.Абельмановская, д.11, кв.99",
+    "г.Москва, ул.Донская, д. 25, стр. 1, кв. 34",
+    "г.Москва, ул.Амурская, вл.3",
+    "г.Москва, ул.Академика Янгеля, д.1, корп.1, кв.145",
+    "г.Москва, ул.Мантулинская, д.9, корп.6, кв. 59",
+    "г.Москва, ул.Библиотечная, д.27, кв. 110"
+  ],
+
+  comment: {'val':'fffff'},
+
+  sum: {'val':0},
+
+  consumption: {'val': ''},
+
+  consumptionOptions: [
+    {'label':'Бонусы менеджеры','needaddress':false},
+    {'label':'Отзывы','needaddress':false},
+    {'label':'ЗП','needaddress':false},
+    {'label':'Налоги','needaddress':false},
+    {'label':'Контекстная реклама','needaddress':true},
+    {'label':'Нужды офиса','needaddress':false},
+    {'label':'Дима','needaddress':false},
+    {'label':'Подрядчики для работы организации','needaddress':false},
+    {'label':'Другая реклама','needaddress':false},
+    {'label':'Аренда офиса','needaddress':false},
+    {'label':'Курьеры','needaddress':false},
+    {'label':'Для работы согласователей','needaddress':true},
+    {'label':'Телефония','needaddress':false},
+    {'label':'Оплата посреднику','needaddress':false},
+    {'label':'Рабочие расходы по объекту','needaddress':false}
+  ]
+}
+};
--- root.js
+++ root.js
@@ -7,10 +7,8 @@ var vueObject = {
   /*html*/
   `
   <div class="q-pa-md fit column justify-center">
-
     <!-- first buttons -->
-    <div class="q-gutter-y-xs">
-
+    <div class="q-gutter-y-xs" style="height:120px">
       <q-btn-toggle
         spread
         v-model = "paymentType.val"
@@ -33,159 +31,178 @@ var vueObject = {
         toggle-color = "accent"
         :options= "placeOptions"
       />
-
     </div>
 
-    <!-- address -->
-    <q-select
-      v-model="address.val"
-      use-input
-      input-debounce="0"
-      label="Адрес"
-      :options="addressOptions"
-      @filter="filteraddressFn"
-      behavior="menu"
-      v-if="showaddress"
-    >
-      <template v-slot:prepend>
-        <q-icon name="location_on" />
-      </template>
-      <template v-slot:no-option>
-        <q-item>
-          <q-item-section class="text-grey">
-            Не нашел такого, ты уверен(а)?
-          </q-item-section>
-        </q-item>
-      </template>
-    </q-select>
-
-    <!-- comment -->
-    <q-input
-      v-model="comment.val"
-      label="Комментарий"
-      autogrow
-    >
-      <template v-slot:prepend>
-        <q-icon name="chat" />
-      </template>
-    </q-input>
+    <div class="q-gutter-y-xs">
+      <!-- comment -->
+      <q-input
+        v-model="comment.val"
+        label="Комментарий"
+        autogrow
+      >
+        <template v-slot:prepend>
+          <q-icon name="chat" />
+        </template>
+      </q-input>
     
-    <!-- sum -->
-    <q-input
-      v-model.number="sum.val"
-      label='Сумма'
-      type="number"
-    >
-      <template v-slot:prepend>
-        <q-icon :name="paymenticon"/>
-      </template>
-    </q-input>
+      <!-- sum -->
+      <q-input
+        v-model.number="sum.val"
+        label='Сумма'
+        type="number"
+      >
+        <template v-slot:prepend>
+          <q-icon :name="paymenticon"/>
+        </template>
+      </q-input>
 
-    <!-- consumption -->
-    <q-select
-      v-model="consumption.val"
-      use-input
-      input-debounce="0"
-      label="Тип расхода"
-      :options="consumptionOptions"
-      @filter="filterconsumptionFn"
-      behavior="menu"
-      v-if="showconsumption"
-    >
-      <template v-slot:prepend>
-        <q-icon name="list" />
-      </template>
-      <template v-slot:no-option>
-        <q-item>
-          <q-item-section class="text-grey">
-            Не нашел такого, ты уверен(а)?
-          </q-item-section>
-        </q-item>
-      </template>
-    </q-select>
+      <!-- consumption -->
+      <q-select
+        v-model="consumption.val"
+        use-input
+        input-debounce="0"
+        label="Тип расхода"
+        :options="consumptionOptions"
+        @filter="filterconsumptionFn"
+        behavior="menu"
+        v-if="showconsumption"
+      >
+        <template v-slot:prepend>
+          <q-icon name="list" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              Не нашел такого, ты уверен(а)?
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
 
+      <!-- address -->
+      <q-select
+        v-model="address.val"
+        use-input
+        input-debounce="0"
+        label="Адрес"
+        :options="addressOptions"
+        @filter="filteraddressFn"
+        behavior="menu"
+        v-if="showaddress"
+      >
+        <template v-slot:prepend>
+          <q-icon name="location_on" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              Не нашел такого, ты уверен(а)?
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+      
+      <!-- save --> 
+      <q-btn color="primary" label="Добавить" class="fit" @click="appendRow()"/>
+    </div>
   </div>
-
   `
   ,
   setup() {
-    const paymentMethod = ref(model.paymentMethod);
-    const paymentType = ref(model.paymentType);
+    const paymentMethod = ref(model.val.paymentMethod);
+    const paymentType = ref(model.val.paymentType);
     const showplace = ref(false);
     const showaddress = ref(false);
-    const addressOptions = ref(model.addressOptions);
+    const addressOptions = ref(model.val.addressOptions);
     const paymenticon = ref('payments');
-    const consumption = ref(model.consumption);
-    const consumptionOptions = ref(model.consumptionOptions.map(item => item.label));
+    const consumption = ref(model.val.consumption);
+    const consumptionOptions = ref(model.val.consumptionOptions.map(item => item.label));
     const showconsumption = ref(false);
-
-    function filteraddressFn (val, update) {
+    
+    function filteraddressFn(val, update) {
       if (val === '') {
         update(() => {
-          addressOptions.value = model.addressOptions;
+          addressOptions.value = model.val.addressOptions;
         })
         return;
       }
       update(() => {
         const needle = val.toLowerCase()
-        addressOptions.value = model.addressOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+        addressOptions.value = model.val.addressOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
       })
     }
 
-    function filterconsumptionFn (val, update) {
+    function filterconsumptionFn(val, update) {
       if (val === '') {
         update(() => {
-          consumptionOptions.value = model.consumptionOptions
+          consumptionOptions.value = model.val.consumptionOptions
         });
         return;
       }
       update(() => {
         const needle = val.toLowerCase()
-        consumptionOptions.value = model.consumptionOptions.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+        consumptionOptions.value = model.val.consumptionOptions.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
       })
     }
 
-    function showplaceFn(){
+    function showplaceFn() {
       showplace.value = paymentMethod.value.val == 'cash' ? true : false;
       paymenticon.value = paymentMethod.value.val == 'cash' ? 'payments' : 'payment';
     }
 
-    function showaddressFn(){
+    function showaddressFn() {
       showaddress.value = paymentType.value.val == 'income' ? true : false;
     }
 
-    function showconsumptionFn(){
+    function showconsumptionFn() {
       showconsumption.value = paymentType.value.val == 'outcome' ? true : false;
     }
 
+    function appendRow() {
+      console.log('appendRow');
+      // model.val = 'origin.val';
+      model.val.paymentMethod.val = 'cashlessпше '
+      // Object.keys(model.val).forEach((item, idx)=>{
+      //   if(model.val[item].hasOwnProperty('val')){
+      //     model.val[item].val = origin.val[item].val
+      //   }
+      // })
+      // vm.$forceUpdate();
+      // vm.$nextTick(() => {
+        // this.renderComponent = true;
+      // });
+      // location.reload();
+    }
+
     watch(consumption.value, (val) => {
-      if(val.val){
+      if (val.val) {
         showaddress.value = val.val.needaddress ? true : false
       }
     })
 
     return {
       paymentMethod,
-      paymentMethodOptions: ref(model.paymentMethodOptions),
+      paymentMethodOptions: ref(model.val.paymentMethodOptions),
       paymentType,
-      paymentTypeOptions: ref(model.paymentTypeOptions),
-      place: ref(model.place),
-      placeOptions: ref(model.placeOptions),
+      paymentTypeOptions: ref(model.val.paymentTypeOptions),
+      place: ref(model.val.place),
+      placeOptions: ref(model.val.placeOptions),
       showplace,
       showplaceFn,
-      address: ref(model.address),
+      address: ref(model.val.address),
       addressOptions,
       showaddress,
       showaddressFn,
       filteraddressFn,
-      comment: ref(model.comment),
-      sum: ref(model.sum),
+      comment: ref(model.val.comment),
+      sum: ref(model.val.sum),
       paymenticon,
       consumption,
       consumptionOptions,
       showconsumption,
       showconsumptionFn,
-      filterconsumptionFn
+      filterconsumptionFn,
+      appendRow
     }
   }
 }
