5a844ec9839648778522f97ae42151a3b5d6ffb6
Damir <d.makhmudov@kazan.csoft.ru>
2021-07-12 14:53:45 +0300

    cooll

--- root.js
+++ root.js
@@ -6,19 +6,191 @@ var vueObject = {
   template:
   /*html*/
   `
-  <comp :key='square'></comp>
-  <q-btn color="primary" label="Добавить" class="fit" @click="forceUp()"/>
+  <div class="q-pa-md fit column justify-center">
+    <!-- first buttons -->
+    <div class="q-gutter-y-xs" style="height:120px">
+      <q-btn-toggle
+        spread
+        v-model = "mega.paymentType.val"
+        toggle-color = "positive"
+        :options= "mega.paymentTypeOptions"
+        v-on:click="showaddressFn();showconsumptionFn()"
+      >
+      </q-btn-toggle>
+
+      <q-btn-toggle
+          spread
+          v-model = "mega.paymentMethod.val"
+          toggle-color = "primary"
+          :options= "mega.paymentMethodOptions"
+          v-on:click="showplaceFn()"
+      >
+      </q-btn-toggle>
+
+      <q-btn-toggle v-if="mega.showplace"
+        spread
+        v-model = "mega.place.val"
+        toggle-color = "accent"
+        :options= "mega.placeOptions"
+      >
+      </q-btn-toggle>
+    </div>
+
+    <!-- fields-->
+    <div class="q-gutter-y-xs">
+      <q-input
+        v-model="mega.comment.val"
+        label="Комментарий"
+        autogrow
+      >
+        <template v-slot:prepend>
+          <q-icon name="chat" />
+        </template>
+      </q-input>
+
+      <q-input
+        v-model.number="mega.sum.val"
+        label='Сумма'
+        type="number"
+      >
+        <template v-slot:prepend>
+          <q-icon :name="mega.paymenticon"/>
+        </template>
+      </q-input>
+
+      <q-select
+        v-model="mega.consumption.val"
+        use-input
+        input-debounce="0"
+        label="Тип расхода"
+        :options="mega.consumptionOptions"
+        @filter="filterconsumptionFn"
+        behavior="menu"
+        v-if="mega.showconsumption"
+      >
+        <template v-slot:prepend>
+          <q-icon name="list" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              Не нашел такого, ты уверен(а)?
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+
+      <q-select
+        v-model="mega.address.val"
+        use-input
+        input-debounce="0"
+        label="Адрес"
+        :options="mega.addressOptions"
+        @filter="filteraddressFn"
+        behavior="menu"
+        v-if="mega.showaddress"
+      >
+        <template v-slot:prepend>
+          <q-icon name="location_on" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              Не нашел такого, ты уверен(а)?
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+      
+      <q-btn color="primary" label="Добавить" class="fit" @click="refreshApp()"/>
+    </div>
+  </div>
   `
   ,
-  data(){
-    return{
-      square: 0
+  setup() {
+    var mega = reactive ({
+      paymentMethod : model.paymentMethod,
+      paymentMethodOptions: model.paymentMethodOptions,
+      paymentTypeOptions: model.paymentTypeOptions,
+      paymentType : model.paymentType,
+      place: model.placeOptions,
+      placeOptions: model.placeOptions,
+      showplace : false,
+
+      address: model.address,
+      addressOptions: model.addressOptions,
+      showaddress : false,
+      
+      paymenticon : 'payments',
+      consumption : model.consumption,
+      consumptionOptions : model.consumptionOptions.map(item => item.label),
+      showconsumption : false,
+      
+      sum: model.sum,
+      comment: model.comment
+    });
+
+    function filteraddressFn(val, update) {
+      if (val === '') {
+        update(() => {
+          mega.addressOptions = model.addressOptions;
+        })
+        return;
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        mega.addressOptions = model.addressOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+      })
     }
-  },
-  methods:{
-    forceUp(){
-      console.log(model);
-      this.square+=1;
+
+    function filterconsumptionFn(val, update) {
+      if (val === '') {
+        update(() => {
+          mega.consumptionOptions = model.consumptionOptions
+        });
+        return;
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        mega.consumptionOptions = model.consumptionOptions.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+      })
+    }
+
+    function showplaceFn() {
+      mega.showplace = mega.paymentMethod.val == 'cash' ? true : false;
+      mega.paymenticon = mega.paymentMethod.val == 'cash' ? 'payments' : 'payment';
+    }
+
+    function showaddressFn() {
+      mega.showaddress= mega.paymentType.val == 'income' ? true : false;
+    }
+
+    function showconsumptionFn() {
+      mega.showconsumption = mega.paymentType.val == 'outcome' ? true : false;
+    }
+
+    function refreshApp() {
+      Object.keys(mega).forEach(key =>{
+        if(mega[key].hasOwnProperty('val')){
+          console.log(key);
+          mega[key].val = ""
+        }
+      });
+      
+    }
+
+    watch(() => mega.consumption.val,(newVal, prevVal) => {
+      mega.showaddress = newVal.needaddress
+    })
+
+    return {
+      mega,
+      showaddressFn,
+      filteraddressFn,
+      showplaceFn,
+      showconsumptionFn,
+      filterconsumptionFn,
+      refreshApp
     }
   }
 }
