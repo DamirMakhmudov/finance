c4456a220a0af93168351dfc3641897e479e4742
Damir <d.makhmudov@kazan.csoft.ru>
2023-01-18 12:50:59 +0300

    bonus edtition

--- data.js
+++ data.js
@@ -1,132 +1,383 @@
-var model = {
+const { createApp, ref, reactive, computed, watch, onMounted, watchEffect, onBeforeUnmount } = Vue;
+const { useQuasar, Loading, QSpinnerGears } = Quasar;
 
-  userOptions: [
-    "Адам Джанкулаев",
-    "Александр Макаров",
-    "Александр Турлаев",
-    "Алексей Калашников",
-    "Алим Таов",
-    "Алина Макарова",
-    "Алина Мартынюк",
-    "Анастасия Блинова",
-    "Анастасия Лупащенко",
-    "Анастасия Сумская",
-    "Анастасия Устинова",
-    "Андрей Ларин",
-    "Андрей Ларин",
-    "Андрей Маслов",
-    "Арам Арутюнян",
-    "Артем Караваев",
-    "Артём Уткин",
-    "Астемир Валиев",
-    "Астемир Гучев",
-    "Батыр Беканов",
-    "Вера Паршина",
-    "Виктор Белоконь",
-    "Владимир Михейкин",
-    "Давид Холофянцев",
-    "Дамир",
-    "Дарья Тимохина",
-    "Дениза Улигова",
-    "Дмитрий Горбунов",
-    "Дмитрий Замятин",
-    "Дмитрий Замятин",
-    "Дмитрий Леонов",
-    "Евгения Светлекова",
-    "Екатерина Винокурова",
-    "Екатерина Николаенко",
-    "Елена Сумская",
-    "Залим Таов",
-    "Игорь Гутчинов",
-    "Ислам Машуков",
-    "Камилла Сохова",
-    "Кирилл Анисимов",
-    "Любовь Куликова",
-    "Максим Джураев",
-    "Марина Бареева",
-    "Марина Дубицкая",
-    "Михаил Вальт",
-    "Михаил Вальт",
-    "Общий М",
-    "Общий С",
-    "Ольга Колиниченко",
-    "Ратмир Кануков",
-    "Робот",
-    "Роман Галахов",
-    "Роман Медведев",
-    "Юлия Емельянова",
-    "Яна Щукина"
-  ],
+var vueObject = {
+  name: "root",
+  template:
+    /*html*/
+    `
+  <div class="q-pa-md fit column justify-center">
+    <!-- first buttons -->
+    <div class="q-gutter-y-xs" style="height:220px">
+      <q-btn-toggle
+        spread
+        v-model = "mega.paymentType.val"
+        toggle-color = "positive"
+        :options= "mega.paymentTypeOptions"
+        v-on:click="showaddressFn();showconsumptionFn()"
+      >
+      </q-btn-toggle>
 
-  paymentType: { "val": "" },
+      <q-btn-toggle style="flex-direction: column"
+        spread
+        v-model = "mega.paymentMethod.val"
+        toggle-color = "primary"
+        :options= "mega.paymentMethodOptions"
+        v-on:click="showofficeFn();"
+      >
+      </q-btn-toggle>
+      
+      <!--
+      <q-btn-toggle
+        spread
+        v-model = "mega.paymentMethod.val"
+        toggle-color = "primary"
+        :options= "mega.paymentMethodOptions"
+        v-on:click="showofficeFn();"
+      >
+      </q-btn-toggle>
+      -->
 
-  paymentTypeOptions: [
-    { label: 'Приход', value: 'Приход' },
-    { label: 'Расход', value: 'Расход' }
-  ],
+      <!--
+      <q-btn-toggle v-if="mega.showoffice"
+        spread
+        v-model = "mega.office.val"
+        toggle-color = "accent"
+        :options= "mega.officeOptions"
+      >
+      </q-btn-toggle>
+      -->
 
-  paymentMethod: { "val": "" },
+    </div>
 
-  paymentMethodOptions: [
-    { label: 'Нал', value: 'Нал' },
-    { label: 'ООО', value: 'ООО' },
-    { label: 'Карта', value: 'Карта' },
-    { label: 'ИП', value: 'ИП' },
-    { label: 'Карта Тинькофф', value: 'Карта Тинькофф' }
-  ],
+    <!-- fields -->
+    <div class="q-gutter-y-xs">
+      <!-- comment -->
+      <q-input
+        v-model="mega.comment.val"
+        label="Комментарий"
+        autogrow
+      >
+        <template v-slot:prepend>
+          <q-icon name="chat" />
+        </template>
+      </q-input>
 
-  office: { "val": "" },
+      <!-- sum -->
+      <q-input
+        v-model="mega.sum.val"
+        label='Сумма'
+        mask='# ### ### ###'
+        reverse-fill-mask
+      >
+        <template v-slot:prepend>
+          <q-icon :name="mega.paymenticon"/>
+        </template>
+      </q-input>
 
-  officeOptions: [
-    { label: 'ВДНХ', value: 'ВДНХ' },
-    { label: 'ВП', value: 'ВП' }
-  ],
+      <!-- consumption -->
+      <q-select
+        v-model="mega.consumption.val"
+        use-input
+        input-debounce="0"
+        label="Тип расхода"
+        :options="mega.consumptionOptions"
+        @filter="filterconsumptionFn"
+        behavior="menu"
+        v-if="mega.showconsumption"
+        >
 
-  address: { "val": '' },
+        <template v-slot:prepend>
+          <q-icon name="list" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              Не нашел такого, ты уверен(а)?
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+      
+      <!-- sheets-->
+      <q-select
+        v-model="mega.sheet.val"
+        use-input
+        input-debounce="0"
+        label="Лист"
+        :options="mega.sheets"
+        behavior="menu"
+        v-if="mega.showaddress"
+      >
+        <template v-slot:prepend>
+          <q-icon name="dynamic_feed" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              Не нашел такого, ты уверен(а)?
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
 
-  addressOptions: [
-    { "label": "г.Москва, ул. Олонецкая, д. 4, кв. 835", "sheet": "Менеджер" },
-    { "label": "г.Москва, ул.Старокачаловская, д.1, к.1, кв.100", "sheet": "Менеджер" },
-    { "label": "г.Москва, ул.Вешняковская, д.10, кв.39", "sheet": "Менеджер" },
-    { "label": "г.Москва, ул.Абельмановская, д.11, кв.99", "sheet": "Менеджер" },
-    { "label": "г.Москва, ул.Донская, д. 25, стр. 1, кв. 34", "sheet": "Менеджер" },
-    { "label": "г.Москва, ул.Амурская, вл.3", "sheet": "Менеджер" },
-    { "label": "г.Москва, ул.Мантулинская, д.9, корп.6, кв. 59", "sheet": "Менеджер" }
-  ],
+      <!-- address-->
+      <q-select
+        v-model="mega.address.val"
+        use-input
+        input-debounce="0"
+        label="Адрес"
+        :options="mega.addressOptions"
+        @filter="filteraddressFn"
+        @filter-abort="abortFilterFn"
+        behavior="menu"
+        v-if="mega.showaddress"
+      >
+        <template v-slot:prepend>
+          <q-icon name="location_on" />
+        </template>
+        <template v-slot:no-option>
+          <q-item>
+            <q-item-section class="text-grey">
+              Не нашел такого, ты уверен(а)?
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
 
-  addressOptionsArchive: [],
+       <!-- manager -->
+       <q-select
+       v-model="mega.manager.val"
+       use-input
+       input-debounce="0"
+       label="Менеджер"
+       :options="mega.userOptions"
+       behavior="menu"
+       @filter="filterusersFn"
+       v-if="mega.showmanager"
+       >
+       <template v-slot:prepend>
+         <q-icon name="list" />
+       </template>
+       <template v-slot:no-option>
+         <q-item>
+           <q-item-section class="text-grey">
+             Не нашел такого, ты уверен(а)?
+           </q-item-section>
+         </q-item>
+       </template>
+     </q-select>
+      
+      <!-- button -->
+      <q-btn color="primary" label="Добавить" class="fit" @click="saveData()" v-if="mega.showbutton"></q-btn>
 
-  comment: { "val": "" },
+    </div>
+  </div>
+  `
+  ,
+  setup() {
+    var mega = reactive({
+      paymentMethod: model.paymentMethod,
+      paymentMethodOptions: model.paymentMethodOptions,
+      paymentTypeOptions: model.paymentTypeOptions,
+      paymentType: model.paymentType,
+      office: model.office,
+      officeOptions: model.officeOptions,
+      showoffice: false,
 
-  sum: { "val": '' },
+      address: model.address,
+      addressOptions: model.addressOptions,
+      showaddress: false,
 
-  consumption: { "val": "" },
+      paymenticon: 'payments',
+      consumption: model.consumption,
+      manager: model.managerApp,
 
-  manager: { "val": "Яна Щукина" },
+      consumptionOptions: model.consumptionOptions.map(item => item.label),
+      userOptions: model.userOptions,
 
-  consumptionOptions: [
-    { 'label': 'Автоматизация', 'needaddress': false },
-    { 'label': 'Аналитика', 'needaddress': false },
-    { 'label': 'Аренда офиса', 'needaddress': false },
-    { 'label': 'Бонусы менеджеры', 'needaddress': false },
-    { 'label': 'Дима', 'needaddress': false },
-    { 'label': 'Для работы согласователей', 'needaddress': true },
-    { 'label': 'Другая реклама', 'needaddress': false },
-    { 'label': 'ЗП', 'needaddress': false },
-    { 'label': 'Контекстная реклама', 'needaddress': false },
-    { 'label': 'Курьеры', 'needaddress': true },
-    { 'label': 'Налоги', 'needaddress': false },
-    { 'label': 'Нужды офиса', 'needaddress': false },
-    { 'label': 'Оплата посреднику', 'needaddress': true },
-    { 'label': 'Отзывы', 'needaddress': true },
-    { 'label': 'Подрядчики для работы организации', 'needaddress': false },
-    { 'label': 'Работы по сайту', 'needaddress': false },
-    { 'label': 'Рабочие расходы по объекту', 'needaddress': true },
-    { 'label': 'СЕО продвижение', 'needaddress': false },
-    { 'label': 'Телефония', 'needaddress': false }
-  ],
+      showconsumption: false,
+      showmanager: false,
 
-  sheets: ['Менеджер', 'Менеджер.Архив'],
+      sum: model.sum,
+      comment: model.comment,
+      showbutton: computed(() => { return (mega.paymentMethod.val && mega.paymentType.val) ? true : false }),
 
-  sheet: { "val": "" }
+      sheet: model.sheet,
+      sheets: model.sheets
+      // sheetselected: computed(() => {return (mega.sheet.val != 'Менеджер.Архив') ? model.addressOptions : model.addressOptionsArchive}),
+    });
+
+    function filteraddressFn(val, update) {
+      if (val === '') {
+        update(() => {
+          mega.addressOptions = mega.sheet.val != 'Менеджер.Архив' ? model.addressOptions : model.addressOptionsArchive;
+        })
+        return;
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        mega.addressOptions = (mega.sheet.val != 'Менеджер.Архив' ? model.addressOptions.filter(v => v.label.toLowerCase().indexOf(needle) > -1) : model.addressOptionsArchive.filter(v => v.label.toLowerCase().indexOf(needle) > -1));
+      })
+    }
+
+    function filterconsumptionFn(val, update, abort) {
+      if (val === '') {
+        update(() => {
+          mega.consumptionOptions = model.consumptionOptions
+        });
+        return;
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        mega.consumptionOptions = model.consumptionOptions.filter(v => v.label.toLowerCase().indexOf(needle) > -1)
+      })
+    }
+
+    function filterusersFn(val, update, abort) {
+      if (val === '') {
+        update(() => {
+          mega.userOptions = model.userOptions
+        });
+        return;
+      }
+      update(() => {
+        const needle = val.toLowerCase()
+        mega.userOptions = model.userOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
+      })
+    }
+
+    function abortFilterFn() {
+      console.log('delayed filter aborted')
+    }
+
+    async function getArchiveAddress() {
+      const url = 'https://script.google.com/macros/s/AKfycbzUgwNF8Tqs3tmw7sV3ZxWKBDN5bUJ2mfr7mUR5MLrWeCMIvo3GSS4ZfKUbYZN5eXRY/exec?key=financeaddress&sheet=Менеджер.Архив';
+      const requestOptions = {
+        method: "GET",
+        // mode: 'no-cors',
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      };
+      let response = await fetch(url, requestOptions);
+      let data = await response.json();
+      model.addressOptionsArchive = data.address;
+    }
+
+    function showofficeFn() {
+      mega.showoffice = mega.paymentMethod.val == 'Нал' ? true : false;
+      mega.paymenticon = mega.paymentMethod.val == 'Нал' ? 'payments' : 'payment';
+    }
+
+    function showaddressFn() {
+      mega.showaddress = mega.paymentType.val == 'Приход' ? true : false;
+    }
+
+    function showconsumptionFn() {
+      mega.showconsumption = mega.paymentType.val == 'Расход' ? true : false;
+    }
+
+    async function saveData() {
+      model.mode = 'finance';
+      model.sum.val = +model.sum.val.replace(/ /g, '');
+      model.consumption.val = model.consumption.val.label;
+      model.address.val = model.address.val.label;
+
+      let model2 = JSON.parse(JSON.stringify(model));
+
+      Object.keys(mega).forEach(key => {
+        if (mega[key].hasOwnProperty('val')) {
+          mega[key].val = ""
+        }
+      });
+
+      mega.sheet.val = 'Менеджер';
+      showofficeFn();
+      showconsumptionFn();
+      showaddressFn();
+
+
+      // var url = 'https://script.google.com/macros/s/AKfycbzUgwNF8Tqs3tmw7sV3ZxWKBDN5bUJ2mfr7mUR5MLrWeCMIvo3GSS4ZfKUbYZN5eXRY/exec';
+      var url = 'https://script.google.com/macros/s/AKfycbxmMJGGfMm8WeOzFsk9gSCgvfE3TtOL1ERB00cQR0waTYtV0AAduN-7X3fgyu59btG9lQ/exec';
+
+      const requestOptions = {
+        method: "POST",
+        // mode: 'no-cors',
+        headers: {
+          // "Content-Type": "application/json",
+          // "Content-Type": "undefined",
+          "Content-Type": "application/x-www-form-urlencoded"
+          // "Content-Type": "text/plain",
+          // 'contentType': "application/json; charset=UTF-8",
+          // 'accept': 'application/json'
+          // 'accept': 'text/plain'
+        },
+        body: JSON.stringify(model2)
+      };
+      let response = await fetch(url, requestOptions);
+
+      // let data = await response.json();
+
+      // fetch(url)
+      // .then(response => {
+      //   return response.json()
+      // })
+      // .then(data =>{
+      //   console.log(data.status)
+      // });
+
+      // fetch(url, requestOptions)
+      //   .then(response => {
+      //     return response.json()
+      //   })
+      //   .then(data =>{
+      //     console.log(data)
+      // });
+    }
+
+    watch(() => mega.consumption.val, (newVal, prevVal) => {
+      mega.showaddress = newVal.needaddress;
+      mega.showmanager = mega.consumption.val.label == 'Бонусы менеджеры' ? true : false;
+    })
+
+    watch(() => mega.sheet.val, (newVal, prevVal) => {
+      mega.addressOptions = newVal != 'Менеджер.Архив' ? model.addressOptions : model.addressOptionsArchive
+    })
+
+    onMounted(() => {
+      getArchiveAddress()
+    })
+
+    return {
+      mega,
+      showaddressFn,
+      filteraddressFn,
+      filterusersFn,
+      filterconsumptionFn,
+      showofficeFn,
+      showconsumptionFn,
+      abortFilterFn,
+      saveData
+    }
+  }
 }
+
+const app = Vue.createApp(vueObject);
+
+app.use(Quasar, {
+  config: {
+    notify: { /* look at QuasarConfOptions from the API card */ },
+    loading: { /* look at QuasarConfOptions from the API card */ }
+  }
+});
+
+Quasar.lang.set(Quasar.lang.ru);
+
+// watch(paymentMethod.value, (val) =>{
+//   console.log(val)
+//   val.val = 'vdnh' ? showoffice=true : showoffice=false
+// })
+// var showoffice = ref(toggelPaymentMethod(paymentMethod))
+// var showoffice = ref(computed(() => { return toggelPaymentMethod(paymentMethod) }));
+// const showoffice = ref(computed(() => { return paymentMethod.value.val == 'cash' ? true : false }));
